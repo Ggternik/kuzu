b0cee5c3751c5e47d4331f2532e46b3d00a92c4c
Guido Galanternik <guidogalanternik@hotmail.com>
2023-05-20 14:56:27 -0300

    asd

---
+++ index.js
@@ -0,0 +1,16 @@
+var kuzurien = parseInt(prompt("¿Cuantas son los monsturien?"));
+
+function vamosAMagma(){
+    alert("Somos wenisimos copamos el mapita");
+}
+
+function sigamosPescando(){
+    alert("Nos falta gente para pelear magma... que vena que estoy");
+}
+
+if (kuzurien >= 7){
+    vamosAMagma();
+}
+else {
+    sigamosPescando();
+    }
